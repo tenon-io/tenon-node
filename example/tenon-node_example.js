dc064c61fe45ac49e0728b79d8614ca67d415d02
Justin Stockton <jstockton@devis.com>
2014-10-09 23:28:16 -0600

    Add basic support for url and src validation

--- example/tenon-node_example.js
+++ example/tenon-node_example.js
@@ -1,6 +1,20 @@
 'use strict';
 
+// Import the module
 var tenonNode = require('../lib/tenon-node.js');
 
-tenonNode.awesome();
-// => awesome
+// Create an instance with your API key
+var tenonApi = new tenonNode('YOUR_API_KEY_HERE');
+
+
+// Examples
+
+// Check a URL
+tenonApi.checkUrl('http://www.google.com', function(err, result) {
+    console.log('Tenon.checkUrl', result);
+});
+
+// Check a block of HTML source code
+tenonApi.checkSrc('<p>Test</p>', function(err, result) {
+    console.log('Tenon.checkSrc', result);
+});
--- test/tenon-node_test.js
+++ test/tenon-node_test.js
@@ -1,10 +1,102 @@
 /*global describe,it*/
 'use strict';
-var assert = require('assert'),
-  tenonNode = require('../lib/tenon-node.js');
 
-describe('tenon-node node module.', function() {
-  it('must be awesome', function() {
-    assert( tenonNode.awesome(), 'awesome');
-  });
+var expect = require('must'),
+    nock = require('nock'),
+    tenonNode = require('../lib/tenon-node.js');
+
+describe('tenon-node module:', function() {
+
+    var API_URL = 'http://beta.tenon.io';
+
+    var api = new tenonNode('AN_API_KEY');
+
+    describe('checkUrl', function() {
+        it('should return an error if a URL is not specified', function() {
+            api.checkUrl('', function(err, result) {
+                expect(err.message).to.be('You must specify a URL to be checked.');
+                expect(result).to.be.empty();
+            });
+        });
+
+        it('A should return an error if Tenon API returns an error message', function() {
+            var url = 'http://www.example.com';
+
+            nock(API_URL)
+                .filteringRequestBody(function() {
+                    return '*';
+                })
+                .post('/api/', '*')
+                .reply(200, {
+                    status: 999,
+                    message: 'a message'
+                });
+
+            api.checkUrl(url, function(err, result) {
+                expect(err).to.be.not.null();
+                expect(err.message).to.be('a message');
+                expect(result).to.be.empty();
+            });
+        });
+
+        it('should return a JSON result object from the Tenon.api', function() {
+            nock(API_URL)
+                .filteringRequestBody(function() {
+                    return '*';
+                })
+                .post('/api/', '*')
+                .reply(200, {
+                    status: 200,
+                    resultSet: 'success!'
+                });
+
+            api.checkUrl('http://www.example.com', function(err, result) {
+                expect(err).to.be.null();
+                expect(result).to.not.be.empty();
+            });
+        });
+    });
+
+    describe('checkSrc', function() {
+        it('should return an error if a block of HTML is not specified', function() {
+            api.checkSrc('', function(err, result) {
+                expect(err.message).to.be('You must specify a block of HTML source code to be checked.');
+                expect(result).to.be.empty();
+            });
+        });
+
+        it('should return an error from the API if something goes wrong', function() {
+            nock(API_URL)
+                .filteringRequestBody(function() {
+                    return '*';
+                })
+                .post('/api/', '*')
+                .reply(200, {
+                    status: 999,
+                    message: 'a message'
+                });
+
+            api.checkSrc('<p>test</p>', function(err, result) {
+                expect(err.message).to.be('a message');
+                expect(result).to.be.empty();
+            });
+        });
+
+        it('should return a JSON result object from the Tenon.api', function() {
+            nock(API_URL)
+                .filteringRequestBody(function() {
+                    return '*';
+                })
+                .post('/api/', '*')
+                .reply(200, {
+                    status: 200,
+                    resultSet: 'success!'
+                });
+
+            api.checkSrc('<p>test</p>', function(err, result) {
+                expect(err).to.be.null();
+                expect(result).to.be.not.null();
+            });
+        });
+    });
 });
